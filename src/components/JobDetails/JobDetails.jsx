ae20fbe128d4916dbb57972bce1173fec74cef83
ar-zani <ahmedrafsanzani@gmail.com>
2023-08-12 11:09:44 +0600

    job details reponsiveness done

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -26,29 +26,28 @@ const JobDetails = () => {
         }
     }
     return (
-        <div>
+        <div className="mt-14 lg:mt-24">
             <h2 className="py-20 bg-slate-50 text-3xl font-bold text-center">Job Details</h2>
-            <div className="flex w-8/12 py-14 m-auto">
-                <div className="w-7/12">
-                    <p className="text-xl font-semibold py-3">Job Description: <span className="text-base font-normal text-gray-400">{jobDetails.job_description}</span></p>
-                    <p className="text-xl font-semibold py-3">Job Responsibility: <span className="text-base font-normal text-gray-400">{jobDetails.job_responsibility}</span></p>
-                    <p className="text-xl font-semibold py-3">Educational Requirements: <br /> <span className="text-base font-normal text-gray-400">{jobDetails.educational_requirements}</span></p>
-                    <p className="text-xl font-semibold py-3">Experiences: <br /> <span className="text-base font-normal text-gray-400">{jobDetails.experiences}</span></p>
+            <div className="lg:flex w-10/12 lg:w-8/12 py-14 m-auto">
+                <div className="lg:w-7/12 m-auto">
+                    <p className="text-md md:text-xl text-justify font-semibold py-3">Job Description: <span className="text-sm md:text-base font-normal text-gray-400">{jobDetails.job_description}</span></p>
+                    <p className="text-md md:text-xl text-justify font-semibold py-3">Job Responsibility: <span className="text-sm md:text-base font-normal text-gray-400">{jobDetails.job_responsibility}</span></p>
+                    <p className="text-md md:text-xl font-semibold py-3">Educational Requirements: <br /> <span className="text-sm md:text-base font-normal text-gray-400">{jobDetails.educational_requirements}</span></p>
+                    <p className="text-md md:text-xl font-semibold py-3">Experiences: <br /> <span className="text-sm md:text-base font-normal text-gray-400">{jobDetails.experiences}</span></p>
                 </div>
-                <div className="h-fit ms-5 w-5/12 bg-slate-100">
+                <div className="mt-5 lg:mt-0 h-fit lg:ms-5 lg:w-7/12 m-auto lg:w-5/12 bg-slate-100">
                     <div className="p-4">
-                    <h4 className="text-xl font-semibold pb-3">Job Details</h4>
+                    <h4 className="text-center lg:text-left text-xl font-semibold pb-3">Job Details</h4>
                     <hr className="border" />
                     <p className="text-lg font-semibold py-3">Salary: <span className="text-base font-normal text-gray-400">{jobDetails.salary} (Per month)</span></p>
                     <p className="text-lg font-semibold pb-3">Job Title: <span className="text-base font-normal text-gray-400">{jobDetails.job_title}</span></p>
-                    <h4 className="text-xl font-semibold py-3">Contact Information</h4>
+                    <h4 className="text-center lg:text-left text-xl font-semibold py-3">Contact Information</h4>
                     <hr className="border" />
                     <p className="text-lg font-semibold py-3">Phone: <span className="text-base font-normal text-gray-400">{jobDetails.contact?.phone}</span></p>
                     <p className="text-lg font-semibold pb-3">Email: <span className="text-base font-normal text-gray-400">{jobDetails.contact?.email}</span></p>
                     <p className="text-lg font-semibold pb-3">Address: <span className="text-base font-normal text-gray-400">{jobDetails.contact?.address}</span></p>
                     </div>
-                    <button onClick={()=> handleAdd(jobDetails.id)} className='w-full mt-4 bg-violet-400 px-4 py-2.5 rounded-md text-white font-semibold text-xl'>Apply Now</button>
-
+                    <button onClick={()=> handleAdd(jobDetails.id)} className='w-full mt-4 bg-violet-400 px-4 py-2.5 rounded-md text-white font-semibold md:text-xl'>Apply Now</button>
                 </div>
             </div>
         </div>
